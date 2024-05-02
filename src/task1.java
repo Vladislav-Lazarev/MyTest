5121a5390d721223a55155bf1154c130e7ad5c7e
Vladislav-Lazarev <168584793+Vladislav-Lazarev@users.noreply.github.com>
2024-05-03 00:27:28 +0400

    myTest

--- src/task1.java
+++ src/task1.java
@@ -2,31 +2,23 @@ import java.util.Scanner;
 
 public class task1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try {
+            int n = Integer.parseInt(args[0]);
+            int m = Integer.parseInt(args[1]);
+            int[] numbers = new int[n];
+            for (int i = 0; i < numbers.length; i++) {
+                numbers[i] = i + 1;
+            }
+            int count = 0;
+            System.out.print("Полученный путь: ");
+            do {
+                System.out.print(numbers[count]);
+                count = (count + m - 1) % n;
+            } while (count != 0);
+            System.out.print(".");
 
-        System.out.print("Введите размер массива -> ");
-        int n = scanner.nextInt();
-
-        System.out.print("Введите длину шага -> ");
-        int m = scanner.nextInt();
-
-        int[] numbers = new int[n];
-
-        for(int i=0; i<numbers.length; i++){
-            numbers[i] = i + 1;
+        }catch(NegativeArraySizeException e){
+            System.out.println("NegativeArraySizeException");
         }
-
-        System.out.print("Круговой массив: ");
-        for(int number:numbers){
-            System.out.print(number);
-        }
-        System.out.println();
-
-        int count = 0;
-        System.out.print("Путь: ");
-        do{
-            System.out.print(numbers[count]);
-            count = (count + m - 1) % n;
-        }while(count != 0);
     }
 }
